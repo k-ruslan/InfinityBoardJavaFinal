07c4d594aa4cbf1596146e41f0356a14abdd64fe
ruslan_khujaniyazov <rkhuzhaniyazov@mail.ru>
2024-01-30 21:20:54 -0800

    Initial commit

---
+++ src/main/java/khj/ruslan/InfinityBoardJavaFinal/InfinityBoardJavaFinalApplication.java
@@ -0,0 +1,14 @@
+package khj.ruslan.InfinityBoardJavaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityBoardJavaFinalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InfinityBoardJavaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/khj/ruslan/InfinityBoardJavaFinal/InfinityBoardJavaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package khj.ruslan.InfinityBoardJavaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityBoardJavaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
